2109550616be52ce2dc83049159d5bf5a9d845b1
Nicolas Chiarello <nicolasch.fcm@gmail.com>
2023-04-08 18:28:56 -0300

    Added infinite scrolling

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,73 @@
-import { useState } from "react";
+import { useState, useRef, useCallback, use, useEffect } from "react";
 import Dashboard from "../components/Layouts/Dashboard";
 import Modal from "../components/modal/Index";
 import parse from "html-react-parser";
 import { ClipLoader } from "react-spinners";
 import { Toaster, toast } from "react-hot-toast";
 import { deleteBlog } from "../utils/api/blogs/blogs.routes";
+import axios from "axios";
 
-const Panel = ({ data }) => {
+const Panel = () => {
   const [modalOpened, setModalOpened] = useState(false);
-  const [blogs, setBlogs] = useState(data);
+  const [blogs, setBlogs] = useState([]);
   const [loader, setLoader] = useState(false);
   const [selected, setSelected] = useState(null);
   const [type, setType] = useState(0);
+  const [page, setPage] = useState(1);
+  const [nbPages, setNbPages] = useState(3);
+
+  console.log({ blogs });
+
+  useEffect(() => {
+    let source = axios.CancelToken.source();
+
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API_URI}/api/blogs?page=${page}`, {
+        cancelToken: source.token,
+      })
+      .then((res) => {
+        setLoader(false);
+        setBlogs((prevBlogs) => {
+          return [...new Set([...prevBlogs, ...res.data.blogs])];
+        });
+      })
+      .catch((err) => {});
+
+    return () => {
+      source.cancel();
+    };
+  }, [page]);
+
   const handletoggle = () => {
     setModalOpened(true);
   };
-  console.log(data);
+
+  const observer = useRef();
+
+  const lastBlogChild = useCallback(
+    (node) => {
+      if (loader) {
+        return;
+      }
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          if (page < nbPages) {
+            setPage((prev) => prev + 1);
+          }
+        }
+      });
+
+      if (node) {
+        observer.current.observe(node);
+      }
+    },
+    [loader]
+  );
+
   return (
     <Dashboard>
       <Toaster />
@@ -42,15 +94,12 @@ const Panel = ({ data }) => {
           </div>
         </div>
         <div className="w-full h-full bg-white shadow-2xl mt-5 rounded-2xl grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 px-6 py-4 overflow-y-scroll scrollbar">
-          {loader ? (
-            <div className="w-full h-full flex items-center justify-center col-span-full">
-              <ClipLoader size={150} />
-            </div>
-          ) : blogs.length > 1 ? (
-            blogs.map((i) => {
+          {blogs.map((i, key) => {
+            if (blogs.length === key + 1) {
               return (
                 <div
                   key={i._id}
+                  ref={lastBlogChild}
                   className="relative w-full h-[calc(25rem)] rounded-[calc(35px)] overflow-hidden shadow-2xl overflow-y-scroll px-5 py-2"
                 >
                   <h3 className="text-xl font-bold my-3">{i.title}</h3>
@@ -65,7 +114,7 @@ const Panel = ({ data }) => {
                     <div
                       onClick={() => {
                         setModalOpened(true);
-                        setType(1)
+                        setType(1);
                         setSelected({ ...i });
                       }}
                       className="px-3 py-2 bg-blue-500 rounded-xl cursor-pointer shadow-lg"
@@ -85,16 +134,56 @@ const Panel = ({ data }) => {
                   </div>
                 </div>
               );
-            })
-          ) : (
-            "No se encontraron publicaciones"
+            } else {
+              return (
+                <div
+                  key={i._id}
+                  className="relative w-full h-[calc(25rem)] rounded-[calc(35px)] overflow-hidden shadow-2xl overflow-y-scroll px-5 py-2"
+                >
+                  <h3 className="text-xl font-bold my-3">{i.title}</h3>
+                  <p className="text-base my-2 font-medium">{i.copete}</p>
+                  <img
+                    className="w-full max-h-60 object-cover my-5"
+                    src={process.env.NEXT_PUBLIC_IMG_URI + "/" + i.img}
+                    alt={i.name}
+                  />
+                  {parse(i.body)}
+                  <div className="absolute h-full py-4 top-0 right-3 flex flex-col gap-2">
+                    <div
+                      onClick={() => {
+                        setModalOpened(true);
+                        setType(1);
+                        setSelected({ ...i });
+                      }}
+                      className="px-3 py-2 bg-blue-500 rounded-xl cursor-pointer shadow-lg"
+                    >
+                      <i className="bx bxs-edit text-2xl text-white"></i>
+                    </div>
+                    <div
+                      onClick={() =>
+                        deleteBlog(setLoader, i._id, setBlogs, () =>
+                          toast.success("Eliminado exitosamente!")
+                        )
+                      }
+                      className="px-3 py-2 bg-red-600 rounded-xl cursor-pointer shadow-lg"
+                    >
+                      <i className="bx bxs-trash text-2xl text-white"></i>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+          })}
+          {loader && (
+            <div className="w-full h-full py-4 col-span-full flex items-center justify-center">
+              <p>Cargando...</p>
+            </div>
           )}
-          {}
         </div>
         {modalOpened && (
           <Modal
             onClose={setModalOpened}
-            blogs={data}
+            blogs={blogs}
             setBlogs={setBlogs}
             setLoader={setLoader}
             initialData={selected}
@@ -105,16 +194,5 @@ const Panel = ({ data }) => {
     </Dashboard>
   );
 };
-
-export async function getServerSideProps() {
-  const getData = await fetch(`${process.env.API_URI}/api/blogs?page=1`);
-  const data = await getData.json();
-
-  return {
-    props: {
-      data: data.blogs,
-    },
-  };
-}
 
 export default Panel;
